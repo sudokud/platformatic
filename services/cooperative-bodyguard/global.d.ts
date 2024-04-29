435f681f9e40fb398978c829e1ed2eb6c559a9d8
Hamza Chetoui <neokyu@Hamzas-MacBook-Air.local>
2024-04-29 18:40:39 +0100

    first commit

--- services/cooperative-bodyguard/global.d.ts
+++ services/cooperative-bodyguard/global.d.ts
@@ -1,9 +1,33 @@
-
-import { FastifyInstance } from 'fastify'
-import { PlatformaticApp, PlatformaticDBConfig, PlatformaticDBMixin, Entities } from '@platformatic/db'
+import type { PlatformaticApp, PlatformaticDBMixin, PlatformaticDBConfig, Entity, Entities, EntityHooks } from '@platformatic/db'
+import { EntityTypes, Movie } from './types'
 
 declare module 'fastify' {
   interface FastifyInstance {
-    platformatic: PlatformaticApp<PlatformaticDBConfig> & PlatformaticDBMixin<Entities>
+    getSchema<T extends 'Movie'>(schemaId: T): {
+      '$id': string,
+      title: string,
+      description: string,
+      type: string,
+      properties: {
+        [x in keyof EntityTypes[T]]: { type: string, nullable?: boolean }
+      },
+      required: string[]
+    };
+  }
+}
+
+interface AppEntities extends Entities {
+  movie: Entity<Movie>,
+}
+
+interface AppEntityHooks {
+  addEntityHooks(entityName: 'movie', hooks: EntityHooks<Movie>): any
+}
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    platformatic: PlatformaticApp<PlatformaticDBConfig> &
+      PlatformaticDBMixin<AppEntities> &
+      AppEntityHooks
   }
 }
